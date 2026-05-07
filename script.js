392eccfd543d381656826cf4daee65663dd9eec8
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-07 23:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
-  // your code here
+  let result =  text.trim();
+
+	if(result.length === 0){
+		return '';
+	}
+  return result[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
